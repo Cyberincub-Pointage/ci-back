39ace122f73bddeecc5e2ed322e6af729b5d0228
steveasterafovo <steveasterafovo@gmail.com>
2026-01-03 15:54:10 +0100

    front cors resolu

--- config/security.js
+++ config/security.js
@@ -4,21 +4,20 @@ module.exports.security = {
     allRoutes: true,
     allowOrigins: process.env.NODE_ENV === 'production'
       ? [
-        'https://cyberincub-staging.netlify.app',
-        'https://www.cyberincub.com'
+        'https://cyberincub.com',
+        'https://www.cyberincub.com',
+        'https://cyberincub-staging.netlify.app'
       ]
       : [
         'http://localhost:3000',
         'http://localhost:3001',
         'http://127.0.0.1:3000'
       ],
-
-    allRoutes: true,
     allowCredentials: true,
     allowAnyOriginWithCredentialsUnsafe: false,
     allowRequestHeaders: 'Content-Type, Authorization, X-Requested-With, x-api-key',
     allowResponseHeaders: 'Content-Type, Authorization, Location, Set-Cookie',
-    allowRequestMethods: 'GET, POST, PUT, PATCH, DELETE,OPTIONS, HEAD'
+    allowRequestMethods: 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD'
   },
 
   // csrf: false
